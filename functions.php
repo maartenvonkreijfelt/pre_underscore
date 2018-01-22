6001f067df965a6d8320780d374cc3cbe14edfcb
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-01-22 16:35:44 +0100

    update

--- functions.php
+++ functions.php
@@ -154,7 +154,26 @@ function pre_underscores_fonts_url() {
 	return esc_url_raw( $fonts_url );
 }
 
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Twenty Seventeen 1.0
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function pre_underscore_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'pre-underscore-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
 
+	return $urls;
+}
+add_filter( 'wp_resource_hints', 'pre_underscore_resource_hint', 10, 2 );
 
 
 
