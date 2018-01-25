692b883343aff98ffb868085c0590627b658aea1
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-01-25 20:07:38 +0100

    update

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -18,10 +18,12 @@
  */
 function pre_underscores_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'pre_underscores_custom_header_args', array(
+		//for default image you could for instance place: get_parent_theme_file_uri( '/assets/images/header.jpg' )
+		//'default-image'          => get_parent_theme_file_uri( '/assets/images/header.jpg' ),
 		'default-image'          => '',
-		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
+		'default-text-color'     => 'ffffff',
+		'width'                  => 2000,
+		'height'                 => 850,
 		'flex-height'            => true,
 		'wp-head-callback'       => 'pre_underscores_header_style',
 	) ) );
