86129060995544309fbba0f04e430ca18ee1d5d2
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-02-02 12:00:42 +0100

    update

--- functions.php
+++ functions.php
@@ -45,7 +45,8 @@ if ( ! function_exists( 'pre_underscores_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'primary' => esc_html__( 'Primary', 'pre_underscores' ),
-					) );
+			'primary' => esc_html__( 'social', 'pre_underscores' ),
+		) );
 
 		/*
 		 * Switch default core markup for search form, comment form, and comments
