54e6c2e6ed266a9d48269c3051db275f999b959f
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-02-04 19:48:20 +0100

    update

--- functions.php
+++ functions.php
@@ -113,6 +113,16 @@ function pre_underscores_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer Widgets', 'pre_underscores' ),
+		'id'            => 'footer-1',
+		'description'   => esc_html__( 'Add footer widgets here.', 'pre_underscores' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
 }
 add_action( 'widgets_init', 'pre_underscores_widgets_init' );
 
