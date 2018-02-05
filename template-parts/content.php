00490c4a1d42b7aea53cdc40b3e817208542d03e
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-02-05 18:59:45 +0100

    update

--- template-parts/content.php
+++ template-parts/content.php
@@ -32,18 +32,11 @@
 
 				<div class="entry-content">
 					<?php
-						the_content( sprintf(
-							wp_kses(
-								/* translators: %s: Name of current post. Only visible to screen readers */
-								__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'pre_underscores' ),
-								array(
-									'span' => array(
-										'class' => array(),
-									),
-								)
-							),
-							get_the_title()
-						) );
+
+					the_excerpt();
+
+
+						
 
 						wp_link_pages( array(
 							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'pre_underscores' ),
