daedcaf55935969dcf0edc75e850beaf1440925f
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-02-05 16:46:46 +0100

    update

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,48 +10,48 @@
 		?>
 
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<header class="entry-header">
-				<?php pre_underscores_the_category_list();?>
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+			<div class="post__content"
+						<header class="entry-header">
+							<?php pre_underscores_the_category_list();?>
+					<?php
+					if ( is_singular() ) :
+						the_title( '<h1 class="entry-title">', '</h1>' );
+					else :
+						the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+					endif;
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php pre_underscores_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
+					if ( 'post' === get_post_type() ) : ?>
+					<div class="entry-meta">
+						<?php pre_underscores_posted_on(); ?>
+					</div><!-- .entry-meta -->
+					<?php
+					endif; ?>
+				</header><!-- .entry-header -->
 
-	<?php pre_underscores_post_thumbnail(); ?>
+				<?php pre_underscores_post_thumbnail(); ?>
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'pre_underscores' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			) );
+				<div class="entry-content">
+					<?php
+						the_content( sprintf(
+							wp_kses(
+								/* translators: %s: Name of current post. Only visible to screen readers */
+								__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'pre_underscores' ),
+								array(
+									'span' => array(
+										'class' => array(),
+									),
+								)
+							),
+							get_the_title()
+						) );
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'pre_underscores' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'pre_underscores' ),
+							'after'  => '</div>',
+						) );
+					?>
+				</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php pre_underscores_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+
+			</div>
 </article><!-- #post-<?php the_ID(); ?> -->
