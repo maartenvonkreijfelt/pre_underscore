9b7a018662555c07f40221ec1d2417a1cdba476b
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-02-06 10:51:56 +0100

    update

--- index.php
+++ index.php
@@ -40,7 +40,11 @@ get_header(); ?>
 
 			endwhile;
 
-			the_posts_navigation();
+			the_posts_pagination( array(
+				'prev_text' => __( 'Newer', 'pre_underscores' ),
+				'next_text' => __( 'Older', 'pre_underscores' ),
+				'before_page_number' => '<span class="screen-reader-text">' . __( 'Page ', 'pre_underscores' ) . '</span>',
+			));
 
 		else :
 
