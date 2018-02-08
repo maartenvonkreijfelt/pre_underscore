e514df2cbb7fa3600e2d794b2206df2706c011ea
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-02-08 11:27:56 +0100

    update

--- footer.php
+++ footer.php
@@ -1,63 +1,54 @@
 <?php
 /**
- * The template for displaying the footer
+ * The template for displaying the footer.
  *
  * Contains the closing of the #content div and all content after.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package Pre_Underscores
+ * @package Humescores
  */
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<?php get_sidebar('footer'); ?>
+<?php get_sidebar( 'footer' ); ?>
 
-	<footer id="colophon" class="site-footer">
+<footer id="colophon" class="site-footer" role="contentinfo">
 
-		
-		
-		
-		
-		
-		<div class="site-footer__wrap">
-			<?php
-			// Make sure there is a social menu to display.
-			if ( has_nav_menu( 'social' ) ) { ?>
-				<nav class="social-menu">
-					<?php
-					wp_nav_menu( array(
-						'theme_location' => 'social',
-						'menu_class'     => 'social-links-menu',
-						'depth'          => 1,
-						'link_before'    => '<span class="screen-reader-text">',
-						'link_after'     => '</span>' . pre_underscores_get_svg( array( 'icon' => 'chain' ) ),
-					) );
-					?>
-				</nav><!-- .social-menu -->
-			<?php } ?>
 
-			<div class="site-info">
-				<div><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'pre_underscores' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'pre_underscores' ), 'WordPress' ); ?></a></div>
-				<div><?php printf( esc_html__( 'Theme: %1$s by %2$s', 'pre_underscores' ), 'pre_underscores', '<a href="https://www.preludio.nl/templates" rel="designer">Preludio</a>' ); ?></div>
-			</div><!-- .site-info -->
-		</div><!-- .site-footer__wrap -->
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	</footer><!-- #colophon -->
+
+
+	<div class="site-footer__wrap">
+		<?php
+		// Make sure there is a social menu to display.
+		if ( has_nav_menu( 'social' ) ) { ?>
+			<nav class="social-menu">
+				<?php
+				wp_nav_menu( array(
+					'theme_location' => 'social',
+					'menu_class'     => 'social-links-menu',
+					'depth'          => 1,
+					'link_before'    => '<span class="screen-reader-text">',
+					'link_after'     => '</span>' . pre_underscores_get_svg( array( 'icon' => 'chain' ) ),
+				) );
+				?>
+			</nav><!-- .social-menu -->
+		<?php } ?>
+
+		<div class="site-info">
+			<div><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'pre_underscores' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'pre_underscores' ), 'Preludio' ); ?></a></div>
+			<div><?php printf( esc_html__( 'Theme: %1$s by %2$s', 'pre_underscores' ), 'pre_underscores', '<a href="http://www.preludio.nl" rel="designer">Preludio</a>' ); ?></div>
+		</div><!-- .site-info -->
+	</div><!-- .site-footer__wrap -->
+
+
+
+
+
+
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
