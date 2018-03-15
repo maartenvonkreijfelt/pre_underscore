9117e3c574a67ff5ff9345303bcb0d57fccb0d12
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-03-15 15:51:03 +0100

    update

--- header.php
+++ header.php
@@ -56,7 +56,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'pre_underscores' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"> <?php echo pre_underscores_get_svg( array( 'icon' => 'arrow-left' ) ) //esc_html_e( 'Primary Menu', 'pre_underscores' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
